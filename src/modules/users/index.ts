da3e5b7d9bdf3f15a5325cd584ba83a15311e6ff
Maël LE PETIT <makibeardy@gmail.com>
2022-01-28 08:33:55 -0500

    Bug fix on User structure

--- src/modules/users/index.ts
+++ src/modules/users/index.ts
@@ -1,3 +1,21 @@
 import { Bytes } from '@graphprotocol/graph-ts'
+import { User } from '../../../generated/schema'
 
-export namespace users {}
+export namespace users {
+  /**
+   * Get (or create if doesn't exist) a specific user
+   * @param userAddress from evm
+   * @returns @type {User}
+   */
+  export function getUser(userAddress: Bytes): User {
+    let accountId = userAddress.toHex()
+
+    let user = User.load(accountId)
+    if (user === null) {
+      user = new User(accountId)
+      user.address = userAddress
+    }
+
+    return user as User
+  }
+}
