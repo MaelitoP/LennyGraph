4abf55d45537b86559df6358ace3795fda0db318
Maël LE PETIT <makibeardy@gmail.com>
2022-01-31 17:29:57 -0500

    Bug fix: can't use callHandler on Polygon

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,6 +3,7 @@ import {
   AddArtistCall,
 } from './../generated/LennyNFT/LennyNFT'
 import { AuctionCreated as AuctionCreatedEvent } from '../generated/Market/Market'
+import { Transfer as TransferEvent } from '../generated/LennyNFT/LennyNFT'
 
 import { users, tokens } from './modules'
 
@@ -13,29 +14,19 @@ import { users, tokens } from './modules'
 export function handleAuctionCreated(event: AuctionCreatedEvent): void {
   let tokenId = event.params.tokenId.toString()
   let token = tokens.getERC721Token(tokenId)
-  token.address = event.params.nftContract
 
+  token.address = event.params.nftContract
   if (event.params.owner) token.owner = event.params.owner.toHex()
 
   token.save()
 }
 
 /**
- * Update {@link ERC721Token entity} URI
- * @param _ {@link SetTokenURICall} inputs
+ *
+ * @param event
  */
-export function handleSetTokenURI(_: SetTokenURICall): void {
-  let tokenId = _.inputs.tokenId
-
-  let token = tokens.getERC721Token(tokenId.toHex())
+export function handleTransfer(event: TransferEvent): void {
+  let token = tokens.getERC721Token(event.params.tokenId.toString())
+  token.owner = event.params.to.toHex()
   token.save()
-}
-
-/**
- * Update isArtist field to true of User entity
- * @param _ {@link AddArtistCall} inputs
- */
-export function handleAddArtist(_: AddArtistCall): void {
-  let user = users.setUserAsArtist(_.inputs.newAddress)
-  user.save()
 }
